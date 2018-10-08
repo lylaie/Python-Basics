de3c7e20affc72c18af075abedccf19cee0b6837
Lylaie <aurelie.d.dumont@gmail.com>
2018-10-08 13:14:38 +0200

    sample tutorial TH

--- Python Lists/list.py
+++ Python Lists/list.py
@@ -1,2 +1,13 @@
-attendees = ["Aurélie", "Benjamin", "Nadja"]
-print('There are', len(attendees), "attendees currently")
+attendees = ["Aurelie", "Benjamin", "Nadja"]
+attendees.append("David")
+attendees.extend(["Sebastien", "Helene"])
+optional_invitees = ["Magda", "Hubert"]
+
+potential_attendees = attendees + optional_invitees
+
+print('There are', len(potential_attendees), "potential attendees currently")
+
+to_line = ", ".join(attendees)
+cc_line = ", ".join(optional_invitees)
+print("To: " + to_line)
+print("Cc: " + cc_line)
